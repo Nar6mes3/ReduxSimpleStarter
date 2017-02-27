77548cd0e9fca3cf228f262bdd2ab6bc1a05e52e
Nar6mes3 <mestres@gmail.com>
2017-02-27 20:37:07 +0100

    Add files via upload
    Initial commit after section 2 from the react course

---
+++ src/components/search_bar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+          term: ''
+      }
+      this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+          <input
+            value={this.state.term}
+            onChange={event => this.handleInputChange(event.target.value)}/>
+      </div>
+
+    );
+  }
+
+  handleInputChange(term) {
+    this.setState({term});
+    this.props.handleSearchTermChange(term);
+  }
+};
+
+export default SearchBar;
